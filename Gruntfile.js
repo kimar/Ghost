6ce313b1b7104e5df1f774f57ce7c4eae117355f
Fabian Becker <halfdan@xnorfz.de>
2014-03-06 16:53:13 +0100

    Compile SASS using node-sass
    closes #5
    - Added node-sass to package.json
    - Added new grunt tasks to compile sass and copy fonts/css to dist
    - Added new folders to .gitignore
    - Added clean task
    - Fixed README

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,4 +15,63 @@ module.exports = function (grunt) {
         return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
     };
 
+    // load all grunt tasks
+    require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Task configuration.
+        clean: {
+            dist: ['dist', 'docs/dist']
+        },
+
+        copy: {
+            fonts: {
+                expand: true,
+                src: 'fonts/*',
+                dest: 'dist/'
+            },
+            docs: {
+                expand: true,
+                cwd: './dist',
+                src: [
+                    '{css,js}/*.min.*',
+                    'css/*.map',
+                    'fonts/*'
+                ],
+                dest: 'docs/dist'
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    'dist/css/<%= pkg.name %>.css': 'sass/screen.scss'
+                }
+            },
+            compress: {
+                options: {
+                    'outputStyle': 'compressed'
+                },
+                files: {
+                    'dist/css/<%= pkg.name %>.min.css': 'sass/screen.scss'
+                }
+            }
+        }
+    });
+
+    // JS distribution task.
+    // grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+    // CSS distribution task.
+    grunt.registerTask('dist-css', ['sass']);
+
+    // Docs distribution task.
+    grunt.registerTask('dist-docs', 'copy:docs');
+
+    // Full distribution task
+    grunt.registerTask('dist', ['clean', 'dist-css', 'copy:fonts', 'dist-docs']);
+    grunt.registerTask('default', 'Build CSS, JS & templates for development', ['dist']);
+
 };
